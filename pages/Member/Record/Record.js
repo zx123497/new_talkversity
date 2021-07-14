290d13dc3868c3f1e3087684def57c03f91879ce
leo <leo000111444@gmail.com>
2021-07-14 15:52:26 +0800

    finish situation page

--- pages/Member/Record/Record.js
+++ pages/Member/Record/Record.js
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
 import { useTheme } from 'react-native-paper';
  const Setting = () => {
      const {colors}=useTheme();
   return (
     <View style={styles(colors).container}>
-      <Text style={styles(colors).text}>Record</Text>
-      <StatusBar style="auto" />
+      
+      {[1,2,3,4,5,6].map((row)=>(
+          <View key={row} style={{flexDirection:"row",height:"15%",backgroundColor:colors.background.paper,borderBottomColor:colors.background.default,borderBottomWidth:1}}>
+          <View style={{flex:1.3,padding:7}}>
+            <View style={{flex:1,backgroundColor:colors.orange.light,borderRadius:5,paddingRight:5,paddingLeft:5}}>
+            <Image
+          style={styles(colors).image}
+          source={require("../../../images/tutor_orange.png")}
+      />
+            </View>
+          
+          </View>
+          
+          <View style={{flex:3,padding:10,justifyContent: 'center',}}>
+            <Text style={{fontSize:15,marginBottom:5,fontWeight:"bold"}}>自我介紹訓練</Text>
+            <Text style={{color:colors.text.secondary}}>June 6, 2021 08:00PM</Text>
+          </View>
+        </View>
+          ))}
     </View>
   );
 }
@@ -17,11 +34,20 @@ const styles =(colors)=> StyleSheet.create({
     flex: 1,
     backgroundColor: colors.background.default,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    overflow:"scroll"
   },
   text:{
     color:colors.text.secondary,
     fontSize:40,
-  }
+  },
+  image:{
+    flex:1,
+    resizeMode: 'contain',
+    width:"100%",
+    padding:2,
+
+
+  },
 });
 export default Setting;
